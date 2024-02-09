0973df788b4ff124df930fc3bb9656b5c9f3c97d
schmeekygeek <ss6087982@gmail.com>
2024-02-10 01:18:17 +0530

    schema

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Server struct {
-
+  clients     []Client
+  rooms       map[string]Room
 }
 
 func (s Server) Serve(w http.ResponseWriter, r http.Request) {
--- types.go
+++ types.go
@@ -1,7 +1,26 @@
 package main
 
+import "net"
+
 type Location struct {
   lat float64
   long float64
 }
 
+type Room struct {
+  c1      Client
+  c2      Client
+  messages  []Message
+}
+
+type Message struct {
+  from    Client
+  body    string
+}
+
+type Client struct {
+  room      string
+  conn      net.Conn
+  location  Location
+  isMatched bool
+}
